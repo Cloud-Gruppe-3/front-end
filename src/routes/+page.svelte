e900b4db6c1c092ff9bee25befe8a608175779df
tijohans <tijohans@stud.ntnu.no>
2023-05-09 21:53:35 +0200

    Added Vcardtojson formatter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { VCardToJSON } from '../utils/vcardparser'
+
+    let fileContents: string = ''
+
+    const handleFileChange = (event: any) => {
+        const target = event.target as HTMLInputElement
+        const file = target.files && target.files[0]
+        
+        if (!file) return
+
+        const reader = new FileReader()
+        reader.onload = (event) => {
+            const contents = event.target?.result as string
+            fileContents = contents
+        }
+
+        reader.readAsText(file)
+    }
+
+    const handleSubmit = async () => {
+        console.log('handlesubmit')
+
+        console.log(fileContents)
+
+        const filedata = await VCardToJSON(fileContents)
+        console.log(filedata)
+    }
+</script>
 
 
-<form action="">
-    <label for="email"></label>
-    <input type="text">
+<section>
+    <h1>Accepting v-cards 😉</h1>
+    <form on:submit|preventDefault={handleSubmit}>
+        <input 
+            on:change={handleFileChange}
+            type="file" 
+            name="file" 
+            id="fileInput" 
+            accept=".vcf"
+            >
 
-    <label for="password"></label>
-    <input type="password">
+        <input type="submit" id="submitButton" value="Send File">
+    </form>
 
-    <input type="submit" value="Login">
-</form>
+    <form action="/api/contacts/zip" method="POST">
+        <label for="downloadButton">Download all vCards in zip archive</label>
+        <input type="submit" value="Download" name="downloadButton">
+    </form>
+
+    <form action="/api/contacts/vcard" method="POST">
+        <label for="downloadButton">Download all vCards as single .vcf file</label>
+        <input type="submit" value="Download" name="downloadButton">
+    </form>
+</section>
