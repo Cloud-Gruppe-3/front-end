47217495e668ea4f31f05d25315c31e7a8df6961
tijohans <tijohans@stud.ntnu.no>
2023-05-09 11:51:15 +0200

    setup frontend with sveltekit and picocss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<form action="">
+    <label for="email"></label>
+    <input type="text">
+
+    <label for="password"></label>
+    <input type="password">
+
+    <input type="submit" value="Login">
+</form>
